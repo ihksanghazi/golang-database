135169f5c737a9ac18005e2570cbde45386595e5
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-21 18:27:32 +0700

    test query sql

--- sql_test.go
+++ sql_test.go
@@ -12,11 +12,35 @@ func TestExecSQL(t *testing.T) {
 
 	ctx:=context.Background()
 
-	query:= "INSERT INTO customer(id,name) VALUES('sandy','SANDY')"
+	query:= "INSERT INTO customer(id,name) VALUES('azhi','AZHI')"
 	_,err:= db.ExecContext(ctx,query)
 	if err != nil{
 		panic(err)
 	}
 
 	fmt.Println("Sukses insert a new customer")
+}
+
+func TestQuerySQL(t *testing.T) {
+	db:= GetConnection()
+	defer db.Close()
+
+	ctx:=context.Background()
+
+	query:= "SELECT id,name FROM customer"
+	rows,err:= db.QueryContext(ctx,query)
+	if err != nil{
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next(){
+		var id,name string
+		err:=rows.Scan(&id,&name)
+		if err != nil{
+			panic(err)
+		}
+		fmt.Println("id : ",id)
+		fmt.Println("name : ",name)
+	}
 }
